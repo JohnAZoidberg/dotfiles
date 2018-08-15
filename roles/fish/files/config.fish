f03139fd6fd65821cea2a8d417361271a2b4d9cb
Daniel Schaefer <git@danielschaefer.me>
2018-08-15 23:03:28 +0200

    New fish functions and fixes

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -7,6 +7,8 @@ fish_vi_key_bindings
 # Fundle plugin manager
 # Install the fish-ssh-agent plugin
 fundle plugin 'tuvistavie/fish-ssh-agent'
+# To source bash files and get environment variables with fish
+fundle plugin 'edc/bass'
 fundle init
 
 # Enable direnv if it is installed
@@ -31,8 +33,9 @@ function rangerd
     popd
 end
 
-# hash a path like nixpkgs expects it
-alias nixhash="nix-hash --type sha256 --flat --base32"
+# Hash a path like nixpkgs expects it - for tarballs you need --unpack
+#alias nixhash="nix-hash --type sha256 --flat --base32"
+alias nixhash="nixos-prefetch-url"
 
 # edit and source this config file
 alias vimfish="vim $HOME/.config/fish/config.fish"
@@ -55,7 +58,7 @@ alias untargz="tar -Ipigz -xf"
 # search for a specific package in a nixpkgs repo
 function grepkg
     pushd $argv[1]
-    git grep "name = \"$argv[2..-1]"
+    git grep -iP "name += \"$argv[2..-1]"
     popd
 end
 
@@ -81,13 +84,12 @@ function nixify
         echo "\
 with import <nixpkgs> {};
 stdenv.mkDerivation {
-  name = "env";
+  name = \"Project\";
   buildInputs = [
     bashInteractive
   ];
 }
 " > default.nix
-        ${EDITOR:-vim} default.nix
     end
 end
 
@@ -112,7 +114,7 @@ alias http="http -s default"
 # Scan for wifi access points with iw
 function scanwifi
     sudo true  # to force password prompt before pipe
-    sudo iw dev wlp2s0 scan | less | grep SSID | sort -u
+    sudo iw dev wlp2s0 scan | less | grep 'SSID:' | sort -u
 end
 
 # Draw graph of all dependencies of a Haskell package
@@ -122,4 +124,27 @@ alias haskellgraph="ghc-pkg dot | tred | dot -Tpdf > pkgs.pdf"
 # Only requirement: nix. Nothing else is installed in your $PATH
 function convertoffice
     nix-shell -p libreoffice --run "libreoffice --headless --convert-to pdf $argv"
+end
+
+# print nix garbage collection roots
+alias nixroots="nix-store --gc --print-roots | less"
+
+# Generate a TOTP like Google Authenticator does
+# You need to provide a 32 bytes Base-32 encoded key
+# like GEZDGNBVGY3TQOJQGEZDGNBVGY3TQOJQ
+function google-auth
+    nix-shell -p oathToolkit --command "oathtool -b --totp=sha1 \"$argv[1]\""
+end
+
+# Enter a nix-shell with a modified stdenv, namely a different gcc
+function nix-shell-gcc
+    nix-shell -E "with import <nixpkgs> {}; (overrideCC stdenv $argv[1]).mkDerivation { name = \"shell-environment\"; buildInputs = [ $argv[2..-1] ]; }"
+end
+
+function nix-build-default
+    nix-build -E "with import <nixpkgs> {}; callPackage ./$argv[1] {}"
+end
+
+function haskellEnv
+  nix-shell -p "haskellPackages.ghcWithPackages (pkgs: with pkgs; [ $argv ])"
 end
