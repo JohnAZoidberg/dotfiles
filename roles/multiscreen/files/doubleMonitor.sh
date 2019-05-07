e1357fab7147d0983f7452622c6fde25d09d9236
Daniel Schaefer <git@danielschaefer.me>
2019-05-07 17:14:15 +0200

    multiscreen: Adjust values for Thinkpad T480

--- roles/multiscreen/files/doubleMonitor.sh
+++ roles/multiscreen/files/doubleMonitor.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-xrandr --output HDMI2 --off \
-       --output DP1 --off \
-       --output DP2 --off \
+xrandr --output HDMI-2 --off \
+       --output DP-1 --off \
+       --output DP-2 --off \
+       --output VIRTUAL-1 --off \
+       --output HDMI-1 --off \
        \
-       --output eDP1 --primary --rotate normal \
-       --mode 1366x768 --pos 304x1080 \
+       --output eDP-1 --primary --rotate normal \
+       --mode 1920x1080 --pos 0x1080 \
        \
-       --output HDMI1 --rotate normal \
+       --output HDMI-2 --rotate normal \
        --mode 1920x1080 --pos 0x0
